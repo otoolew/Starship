34dae44066b6fe135cce1f3c1e6a9c2971e1480f
otoolew <otoolew@gmail.com>
2018-10-02 19:06:03 -0400

    Implemented Weapon HUD Countdown Timer

---
+++ Assets/Code/UI/HUD/WeaponSlot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// TODO: Needs to be more robust.
+/// </summary>
+public class WeaponSlot : MonoBehaviour
+{
+    public int slot;
+    public Text weaponName;
+    public Text weaponCooldown;
+    public WeaponController weaponController;
+    // Use this for initialization
+    void Start ()
+    {
+        weaponController = FindObjectOfType<WeaponController>();
+        weaponName.text = weaponController.WeaponList[slot].WeaponName;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        weaponCooldown.text = ""+weaponController.WeaponList[slot].WeaponTimer;
+    }
+}
